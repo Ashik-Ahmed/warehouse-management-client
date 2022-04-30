2b0c7b24b3594c6a4c5548bfacef44466ae7b57f
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-04-30 17:38:10 +0600

    add item to db works

--- src/components/AddItems/AddItems.js
+++ src/components/AddItems/AddItems.js
@@ -1,13 +1,41 @@
 import React from 'react';
 
 const AddItems = () => {
+
+
+    const handleAddProduct = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const price = e.target.price.value;
+        console.log(name, price)
+
+        const product = { name, price };
+
+        //send data to the server
+        fetch('http://localhost:5000/add', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert("Product added successfully");
+                    e.target.reset();
+                }
+            })
+    }
+
+
     return (
         <div className='mt-16'>
             <h2>Adding Items</h2>
-            <form>
-                <input type="text" placeholder='Name' />
+            <form onSubmit={handleAddProduct}>
+                <input type="text" name='name' placeholder='Name' />
                 <br />
-                <input type="text" placeholder='Price' />
+                <input type="text" name='price' placeholder='Price' />
                 <br />
                 <input className='bg-blue-300 p-3 cursor-pointer' type="submit" value="Add" />
             </form>
