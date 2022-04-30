5123c430a7a3a0fbc52aedce5b89ea82d324affb
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-04-30 17:55:05 +0600

    get all products api added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddItems from './components/AddItems/AddItems';
 import Login from './components/Login/Login';
 import Signup from './components/Login/Signup';
+import ManageItems from './components/ManageItems/ManageItems';
 import Navbar from './components/Shared/Navbar/Navbar';
 
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
         <Route path='/add' element={<AddItems></AddItems>}></Route>
+        <Route path='/manage' element={<ManageItems></ManageItems>}></Route>
       </Routes>
 
 
---
+++ src/components/ManageItems/ManageItems.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const ManageItems = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+
+    return (
+        <div className='mt-16'>
+            <h2 className='font-bold'>Available Products: {products.length}</h2>
+            {
+                products.map(product => <><p> <span>{product.name}</span> - <span>{product.price}</span></p></>)
+            }
+        </div>
+    );
+};
+
+export default ManageItems;
